f897369a611afef5f4f85ea7c67f95f0c20ce1e0
Vinod <vinod.hire.csn@gmail.com>
2018-02-18 17:26:31 +0530

    Updating application file

--- SpringBootExamples/SpringDataREST/src/main/java/com/vinod/Application.java
+++ SpringBootExamples/SpringDataREST/src/main/java/com/vinod/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) throws Exception {
+		System.out.println("This is Spring Boot Applicatio");
 		SpringApplication.run(Application.class, args);
 	}
 
